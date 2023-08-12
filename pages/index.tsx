18ecc7cd25d191cfd608247c55065d0d9691c9d5
omarfrihi <omar.frihi@gmail.com>
2023-08-12 12:52:29 +0100

    preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>UNFM</h1>
+        <h1 className={styles.title}>UNFM Preview</h1>
       </main>
     </div>
   );
