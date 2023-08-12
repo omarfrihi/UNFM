f5987425c691a4f0ba78819fd33d4d23d97a5f5e
omarfrihi <omar.frihi@gmail.com>
2023-08-13 00:51:51 +0100

    add fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,21 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>UNFM APP</title>
+        <title>UNFM</title>
         <meta name="description" content="unfm" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>UNFM</h1>
+        <h1 className={styles.title_gilroy}>
+          Whereas disregard and contempt for human rights have resulted
+        </h1>
+        <h1 className={styles.title_poppins}>
+          Whereas disregard and contempt for human rights have resulted
+        </h1>
+        <h1 className={styles.title_tajawal}>
+          Whereas disregard and contempt for human rights have resulted
+        </h1>
       </main>
     </div>
   );
