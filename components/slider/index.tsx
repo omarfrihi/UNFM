b4019d3a6be0c2c83d996bfab37e5f1b711e5399
omarfrihi <omar.frihi@gmail.com>
2023-08-13 23:50:26 +0100

    add navbar & slider

--- components/slider/index.tsx
+++ components/slider/index.tsx
@@ -25,7 +25,7 @@ const Banner = () => {
       </div>
       <div>
         <Image
-          src={require("../../public/assets/banner2.png")}
+          src={require("../../public/assets/banner1.png")}
           alt="banner1"
           layout="responsive"
           loading="lazy"
