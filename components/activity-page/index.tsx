8595f3704ca3e38d9907cba33496ee01d21727d1
omarfrihi <omar.frihi@gmail.com>
2023-08-20 04:11:18 +0100

    add activities

--- components/activity-page/index.tsx
+++ components/activity-page/index.tsx
@@ -1,5 +1,47 @@
+import { MenuItem, OutlinedInput } from "@mui/material";
 import { ActivityComponent } from "../activities";
-import { ActivityWrapper, SingleActivity, Wrapper } from "./styles";
+import {
+  ActivityWrapper,
+  Arrow,
+  Filter,
+  Search,
+  SearchLogo,
+  SelectFilter,
+  SingleActivity,
+  Wrapper,
+} from "./styles";
+
+const Select = ({ title, options }: { title: string; options: string[] }) => (
+  <SelectFilter
+    value="value"
+    sx={{
+      "& .MuiOutlinedInput-notchedOutline": {
+        border: "2px solid #144056 !important",
+      },
+      "&:hover > .MuiOutlinedInput-notchedOutline": {
+        border: "2px solid #144056 !important",
+      },
+      "&:focus > .MuiOutlinedInput-notchedOutline": {
+        border: "2px solid #144056 !important",
+      },
+      ".Mui-focused .MuiOutlinedInput-notchedOutline": {
+        border: "2px solid #144056 !important",
+      },
+      ".MuiSelect-iconOutlined": {
+        color: "#144056",
+      },
+    }}
+  >
+    <MenuItem value={"value"} disabled>
+      {title}
+    </MenuItem>
+    {options.map((option) => (
+      <MenuItem value={option} key={option}>
+        {option}
+      </MenuItem>
+    ))}
+  </SelectFilter>
+);
 const activities = [
   {
     image: require("../../public/assets/activity1.png"),
@@ -56,6 +98,41 @@ const activities = [
 ];
 const Activity = () => (
   <Wrapper>
+    <Filter>
+      <Search
+        placeholder="Rechercher"
+        startAdornment={<SearchLogo fontSize="small" />}
+        sx={{
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+          },
+          "&:hover > .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+          },
+        }}
+      />
+      <Select
+        title="type d'activité"
+        options={["activité 1", "activité 2", "activité 3", "activité 4"]}
+      />
+      <Select
+        title="type de programme"
+        options={["programme 1", "programme 2", "programme 3", "programme 4"]}
+      />
+      <Select
+        title="Associations accréditées"
+        options={[
+          "Associations 1",
+          "Associations 2",
+          "Associations 3",
+          "Associations 4",
+        ]}
+      />
+      <Select
+        title="Journées de le Femme"
+        options={["Journées 1", "Journées 2", "Journées 3", "Journées 4"]}
+      />
+    </Filter>
     <ActivityWrapper>
       {activities.map((activity) => (
         <SingleActivity key={activity.title}>
--- components/activity-page/styles.ts
+++ components/activity-page/styles.ts
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
+import { Input, OutlinedInput, Select, TextField } from "@mui/material";
 import { responsive } from "../../styles/mixins";
-
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 export const Wrapper = styled("div")`
   display: flex;
   flex-direction: column;
@@ -25,4 +27,54 @@ export const SingleActivity = styled.div`
   flex-basis: 100%;
 
   `}
+`;
+export const Filter = styled("div")`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+`;
+
+export const Search = styled(OutlinedInput)`
+  border-radius: 6.5rem;
+  background: ${({ theme }) => theme.colors.white[200]};
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.08);
+  border: none;
+  height: 2.3rem;
+  width: 15rem;
+  input {
+    padding: 0.5rem;
+    font-family: Gilroy;
+    font-size: 0.8rem;
+    ::placeholder {
+      color: rgba(17, 17, 17, 0.48);
+      font-family: Gilroy;
+      font-size: 0.8rem;
+      font-style: normal;
+      font-weight: 500;
+      text-transform: capitalize;
+    }
+  }
+`;
+
+export const SelectFilter = styled(Select)`
+  border-radius: 6.5rem;
+  height: 2.3rem;
+  & .MuiOutlinedInput-input {
+    color: rgba(17, 17, 17);
+    font-family: Gilroy;
+    font-size: 0.8rem;
+    color: ${({ theme }) => theme.colors.blue[100]};
+    font-family: Gilroy;
+    font-style: normal;
+    font-weight: 600;
+    text-transform: capitalize;
+  }
+`;
+
+export const SearchLogo = styled(SearchIcon)`
+  color: rgba(17, 17, 17, 0.48);
+`;
+
+export const Arrow = styled(ArrowDropDownIcon)`
+  color: ${({ theme }) => theme.colors.blue[100]};
 `;
