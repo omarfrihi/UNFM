bb3223748895242e17090a78338e5b32ba6dab4c
omarfrihi <omar.frihi@gmail.com>
2023-09-05 22:43:48 +0100

    commit

--- components/test/history.tsx
+++ components/test/history.tsx
@@ -1,66 +1,68 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Timeline from "timelinejs-react";
+import { HistoryWrapper } from "./styles";
 
 const events: Slide[] = [
   {
     start_date: {
-      year: 2021,
+      year: 1990,
       month: 6,
       day: 5,
     },
     media: {
-      url: "https://picsum.photos/200/300",
-      thumbnail: "https://picsum.photos/200/300",
+      url: "https://s3-alpha-sig.figma.com/img/a710/6344/e404c4a9a83b26d0dc67dc51d4073884?Expires=1694995200&Signature=ic8f8EPtOmg2K-Wctx~3NrHpoI-LdK66--WEY8ILkAqqYC1UOdcdlLG0i5z8PRU5QcdCJZpFCmgFv-FfbpvWZoTxdEeX8no39-ffnnUA2QreoJrUAxDmeo6LHj4gT1BqPlbQXgrun0GKOUC-giBBaEfXUrAqAiu7PiuyWKJcB2KxDkj205799nh6PeyjaUGtvFBoL3~pqF1c9hE2Op~q8XrpNmHmuCe0XJo7KPJy6hNgj8CNxB2pf7Da8naomUhiV9NDY2n-t5aYCrxWIJslkwOVNlu-plaPSNieExvpxDNNUmOdDsF-pHzCWNlKhcKsZDaVKZrvgYJ3ZlfHwzFVjA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      thumbnail:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
       caption: "",
       link: "",
     },
     end_date: {
-      year: 2021,
-      month: 7,
-      day: 4,
+      year: 1990,
+      month: 6,
+      day: 5,
     },
     unique_id: "1",
     text: {
-      headline: "Event1",
-      text: "",
+      headline: "Premier centre d’appel et actions dans les 16 régions",
+      text: "Dès la première année de l’existence de l’UNFM, Sa Présidente et les nombreuses collaboratrices de l’Union sont actives sur plusieurs fronts et dans les 16 régions du Royaume, multipliant les innovations, comme le premier centre d’appel à Settat, et les initiatives, notamment des campagnes de sensibilisation à la santé maternelle ou des cours pour lutter contre l’analphabétisme.",
     },
-    group: "Catégorie1",
     background: {},
   },
   {
     start_date: {
-      year: 2021,
+      year: 1995,
       month: 8,
       day: 4,
     },
     media: {
       url: "https://picsum.photos/200/300",
-      thumbnail: "https://picsum.photos/200/300",
+      thumbnail:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
       caption: "",
       link: "",
     },
     end_date: {
-      year: 2021,
-      month: 9,
-      day: 5,
+      year: 1995,
+      month: 8,
+      day: 4,
     },
     unique_id: "2",
     text: {
       headline: "Event2",
-      text: "",
+      text: "MMMMM",
     },
-    group: "Catégorie1",
     background: {},
   },
   {
     start_date: {
       year: 2021,
       month: 8,
-      day: 4,
+      day: 5,
     },
     media: {
       url: "https://picsum.photos/200/300",
-      thumbnail: "https://picsum.photos/200/300",
+      thumbnail:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
       caption: "",
       link: "",
     },
@@ -74,25 +76,34 @@ const events: Slide[] = [
       headline: "Event3",
       text: "",
     },
-    group: "Catégorie2",
     background: {},
   },
 ];
 
 const MyComponent: React.FC = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    console.log(ref);
+  }, [ref]);
   return (
-    <Timeline
-      target={<div className="timeline" style={{ height: 600 }} />}
-      events={events}
-      options={{
-        timenav_position: "top",
-        hash_bookmark: true,
-        initial_zoom: 1,
-        scale_factor: 1,
-        debug: true,
-        default_bg_color: "green",
-      }} // optional
-    />
+    <HistoryWrapper>
+      <Timeline
+        ref={ref}
+        target={<div className="timeline" style={{ height: 600 }} />}
+        events={events}
+        options={
+          {
+            // timenav_position: "top",
+            // hash_bookmark: true,
+            // initial_zoom: 1,
+            // scale_factor: 1,
+            // debug: true,
+            // default_bg_color: "green",
+          }
+        } // optional
+      />
+    </HistoryWrapper>
   );
 };
 
---
+++ components/test/styles.ts
@@ -0,0 +1,120 @@
+import styled from "@emotion/styled";
+
+export const HistoryWrapper = styled.div`
+  & .tl-slide {
+    background: linear-gradient(180deg, #f2f2f2 0%, rgba(255, 255, 255, 0) 100%),
+      #fff;
+  }
+  & .tl-media .tl-media-content-container .tl-media-content img {
+    height: 370px;
+    max-width: 500px;
+  }
+  & .tl-timemarker .tl-timemarker-content-container {
+    height: fit-content !important;
+    width: fit-content !important;
+    border-radius: 50%;
+    transform: translateX(-21px);
+    border-color: #144056;
+  }
+
+  & .tl-timemarker.tl-timemarker-active .tl-timemarker-content-container {
+    border-color: #31dc84;
+  }
+
+  &
+    .tl-timemarker
+    .tl-timemarker-content-container
+    .tl-timemarker-content
+    .tl-timemarker-text {
+    display: none;
+  }
+
+  &
+    .tl-timemarker
+    .tl-timemarker-content-container
+    .tl-timemarker-content
+    .tl-timemarker-media-container {
+    margin: 0;
+    height: 40px;
+    width: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    max-width: unset;
+    max-height: unset;
+  }
+
+  & .tl-timemarker .tl-timemarker-content-container .tl-timemarker-content {
+    padding: 0;
+  }
+  & .tl-timemarker .tl-timemarker-line-left {
+    border-color: #144056;
+  }
+  & .tl-timemarker.tl-timemarker-active .tl-timemarker-line-left {
+    border-color: #31dc84;
+  }
+  &
+    .tl-timemarker
+    .tl-timemarker-content-container
+    .tl-timemarker-content
+    .tl-timemarker-media-container
+    .tl-timemarker-media {
+    max-width: unset;
+    opacity: 1;
+  }
+  & .tl-timemarker.tl-timemarker-active .tl-timemarker-line-left:after {
+    background-color: #31dc84;
+  }
+
+  & .tl-timemarker .tl-timemarker-line-left:after {
+    background-color: #144056;
+  }
+  & .tl-timeline .tl-headline-date,
+  & .tl-timeline h3.tl-headline-date {
+    color: #31dc84;
+    font-family: Red Hat Display;
+    font-size: 1.3rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    text-transform: capitalize;
+  }
+  & .tl-timeline h1,
+  .tl-timeline h2,
+  .tl-timeline h3,
+  .tl-timeline h4,
+  .tl-timeline h5,
+  .tl-timeline h6 {
+    color: #144056;
+    font-family: Red Hat Display;
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -0.03563rem;
+    text-transform: capitalize;
+    margin-bottom: 1rem;
+  }
+
+  & .tl-timeline p {
+    color: #144056;
+    text-align: justify;
+    font-family: Red Hat Display;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+  & .tl-slide .tl-slide-content-container .tl-slide-content {
+    display: flex;
+    justify-content: center;
+    gap: 5rem;
+    align-items: center;
+  }
+  & .tl-slide .tl-slide-content-container .tl-slide-content .tl-media {
+    min-width: unset;
+    max-width: unset;
+    width: fit-content;
+  }
+`;
